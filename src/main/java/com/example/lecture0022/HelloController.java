521dfa4a9d451634402fabae4a61609903e82b23
parkchaewoon53 <parkchae53@gmail.com>
2025-05-26 11:42:29 +0900

    feat : 두번째 commit

---
+++ src/main/java/com/example/lecture0022/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.lecture0022;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("hello")
+	public ResponseEntity<String> hello(){
+		return ResponseEntity.ok("hello");
+	}
+}

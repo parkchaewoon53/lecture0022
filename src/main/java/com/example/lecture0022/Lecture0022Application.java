8e73b8276fbf5dc3ae532fb61b9459f3fcda8f0a
parkchaewoon53 <parkchae53@gmail.com>
2025-05-26 11:29:19 +0900

    First commit

---
+++ src/main/java/com/example/lecture0022/Lecture0022Application.java
@@ -0,0 +1,13 @@
+package com.example.lecture0022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture0022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture0022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lecture0022/Lecture0022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture0022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture0022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
